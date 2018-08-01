6ae929af216b5da3345287d34977181ca8a4860c
Ian Turner <vectro@vectro.org>
2018-08-01 17:47:30 -0400

    Add more tests for JsonTransformer, for the different data types and formats.

--- src/test/java/com/living_goods/couch2sql/JsonTransformerTest.java
+++ src/test/java/com/living_goods/couch2sql/JsonTransformerTest.java
@@ -2,9 +2,10 @@ package com.living_goods.couch2sql;
 
 import org.junit.Test;
 import org.lightcouch.ChangesResult.Row;
-import org.everit.json.schema.ValidationException;
 import com.google.gson.JsonObject;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.schibsted.spt.data.jslt.JsltException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -70,14 +71,13 @@ public class JsonTransformerTest {
     
     /* Test epoch datetime transform. */
     @Test
-    public void testDateTimeEpoch() {
+    public void testDateTime1() {
         PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
         JsonTransformer jt = new JsonTransformer(p);
         JsonObject testObj = makePerson();
         testObj.addProperty("reported_date", "1514764800000");
         Row r = new RowMock("", testObj);
         jt.send(r);
-        System.out.println(p.getSent());
         assertEquals("2018-01-01 00:00:00.000",
                      p.getSent().get(0).getResult().get("row")
                      .get("REPORTED_DATE").asText());
@@ -85,15 +85,126 @@ public class JsonTransformerTest {
     }
     
     /* Test yyyymmdd or yyyymmmdd transform. */
+    @Test
+    public void testDateTime2() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+
+        JsonObject testObj = makePerson();
+        testObj.addProperty("date_of_birth", "2018-01-02");
+        jt.send(new RowMock("", testObj));
+
+        testObj = makePerson();
+        testObj.addProperty("date_of_birth", "20180102");
+        jt.send(new RowMock("", testObj));
+
+        testObj = makePerson();
+        testObj.addProperty("date_of_birth", "2018-Jan-02");
+        jt.send(new RowMock("", testObj));
+
+        for (TransformedChange tc : p.getSent()) {
+            assertEquals("2018-01-02",
+                         tc.getResult().get("row").get("DATE_OF_BIRTH")
+                         .asText());
+        }
+    }
 
     /* Test ddmmyyyy or yyyymmdd transform. */
+    @Test
+    public void testDateTime3() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+
+        JsonObject testObj = makePerson();
+        testObj.addProperty("date_of_graduation", "2018-01-20");
+        jt.send(new RowMock("", testObj));
+
+        testObj = makePerson();
+        testObj.addProperty("date_of_graduation", "20180120");
+        jt.send(new RowMock("", testObj));
+
+        testObj = makePerson();
+        testObj.addProperty("date_of_graduation", "01/20/2018");
+        jt.send(new RowMock("", testObj));
+
+        for (TransformedChange tc : p.getSent()) {
+            assertEquals("2018-01-20",
+                         tc.getResult().get("row").get("DATE_OF_GRADUATION")
+                         .asText());
+        }
+    }
 
     /* Test equal values validation (pass). */
+    @Test
+    public void testIdenticalPass() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+        JsonObject testObj = makePerson();
+        testObj.addProperty("relationship_to_primary_caregiver", "self");
+        testObj.addProperty("relationship_primary_caregiver", "self");
+
+        Row r = new RowMock("", testObj);
+        jt.send(r);
+        assertEquals("self",
+                     p.getSent().get(0).getResult().get("row")
+                     .get("RELATIONSHIP_TO_PRIMARY_CAREGIVER").asText());
+    }
 
     /* Test equal values validation (fail). */
+    @Test(expected = JsltException.class)
+    public void testIdenticalFail() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+        JsonObject testObj = makePerson();
+        testObj.addProperty("relationship_to_primary_caregiver", "daughter");
+        testObj.addProperty("relationship_primary_caregiver", "son");
+
+        Row r = new RowMock("", testObj);
+        jt.send(r);
+    }
 
     /* Test suppression of empty strings. */
+    @Test
+    public void testAbsentValues() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+        JsonObject testObj = makePerson();
+        testObj.addProperty("first_name", "");
+        testObj.addProperty("phone", "");
+        testObj.addProperty("reported_date", "");
+        testObj.addProperty("date_of_graduation", "");
+        testObj.addProperty("date_of_birth", "");
+        testObj.addProperty("pregnant_at_registration", "");
+
+        /* We should get the same result as we would have gotten
+         * without adding all these empty fields. */
+        jt.send(new RowMock("", testObj));
+        jt.send(new RowMock("", makePerson()));
+        assertEquals(p.getSent().get(1).getResult(),
+                     p.getSent().get(0).getResult());
+    }
 
     /* Test boolean transforms */
+    @Test
+    public void testBoolean() {
+        PipedMock<TransformedChange> p = new PipedMock<TransformedChange>();
+        JsonTransformer jt = new JsonTransformer(p);
+        JsonObject testObj = makePerson();
+        testObj.addProperty("pregnant_at_registration", "true");
+        testObj.addProperty("fp_eligible", "yes");
+        testObj.addProperty("delivered_in_facility", true);
+        testObj.addProperty("exclusively_breastfed", "false");
+        testObj.addProperty("slept_under_treated_net", "no");
+        testObj.addProperty("cant_pay_now", false);
+
+        jt.send(new RowMock("", testObj));
+        JsonNode row = p.getSent().get(0).getResult().get("row");
+        assertEquals(BooleanNode.TRUE, row.get("PREGNANT_AT_REGISTRATION"));
+        assertEquals(BooleanNode.TRUE, row.get("FP_ELIGIBLE"));
+        assertEquals(BooleanNode.TRUE, row.get("DELIVERED_IN_FACILITY"));
+        assertEquals(BooleanNode.FALSE, row.get("EXCLUSIVELY_BREASTFED"));
+        assertEquals(BooleanNode.FALSE, row.get("SLEPT_UNDER_TREATED_NET"));
+        assertEquals(BooleanNode.FALSE, row.get("CANT_PAY_NOW"));
+    }
 
 }
