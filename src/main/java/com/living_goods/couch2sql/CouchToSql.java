135203df5615d8040b8882949739bab21bc1e100
Ian Turner <vectro@vectro.org>
2018-08-06 13:10:20 -0400

    Initial framework for running under winrun4j.

--- src/main/java/com/living_goods/couch2sql/CouchToSql.java
+++ src/main/java/com/living_goods/couch2sql/CouchToSql.java
@@ -1,13 +1,51 @@
 package com.living_goods.couch2sql;
 
-public class CouchToSql
-{
-    public static void main( String[] args )
+import java.lang.Runnable;
+import org.boris.winrun4j.Service;
+import org.boris.winrun4j.EventLog;
+import org.boris.winrun4j.ServiceException;
+
+public class CouchToSql implements Service, Runnable {
+    public static void main(String[] args)
     {
-        final SqlWriter writer = new SqlWriter();
+        CouchToSql instance = new CouchToSql();
+        instance.run();
+    }
+
+    @Override
+    public int serviceMain(String[] args) {
+        run();
+        return 0; /* Ignored? */
+    }
+
+    @Override
+    public int serviceRequest(int control) {
+        switch (control) {
+        case SERVICE_CONTROL_STOP:
+        case SERVICE_CONTROL_SHUTDOWN:
+            stop();
+            break;
+        }
+        return 0; /* Ignored? */
+    }
+    
+    private SqlWriter writer;
+    private CouchReader reader;
+    
+    CouchToSql() {
+        writer = new SqlWriter();
         final JsonTransformer transformer = new JsonTransformer(writer);
         final JsonValidator validator = new JsonValidator(transformer);
-        final CouchReader reader = new CouchReader(validator);
+        reader = new CouchReader(validator);
+        reader.setSeq(writer.getSeq());
+    }
+
+    @Override
+    public void run() {
         reader.run();
+    }
+
+    public void stop() {
+        reader.stop();
     }
 }
